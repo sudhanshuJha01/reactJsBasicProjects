599a0a7811acd88a76557259fa3d583b0f2e2ec4
sudhanshuJha01 <sudhanshujha883@gmail.com>
2024-06-05 20:41:01 +0530

    one last try

--- 06_shoppingCart/src/components/NavBar.jsx
+++ 06_shoppingCart/src/components/NavBar.jsx
@@ -10,7 +10,7 @@ function NavBar() {
                 <Link to={'/'}>Shop</Link>
                 <Link to={'/cart'}><ShoppingCart size={32} /></Link>
                 </div>
-                
+                   
         </div>
         </>
     )
